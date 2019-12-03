02041e5f33349d2566a8cabf73a23dc4b4469371
artempronozin95 <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 12:34:25 +0700

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-#OUT='./protein'
+OUT='./protein'
 OUT_DIR='./tab'
-#wget -O $OUT/$1.fasta http://rest.kegg.jp/get/ath:$1/aaseq
+wget -O $OUT/$1.fasta http://rest.kegg.jp/get/ath:$1/aaseq
 
 wget 'https://v100.orthodb.org/tab?query='$1'&level=2759&species=2759&universal=all' -O $OUT_DIR/$1.csv
 a=$(awk -F '\t' 'NR==2{print $1}' $OUT_DIR/$1.csv)
