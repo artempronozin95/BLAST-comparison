6dd4a4a7f2341923ceea585fae907f47a79d2c6d
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 16:28:03 +0700

    Update column.py

--- column.py
+++ column.py
@@ -4,7 +4,7 @@ import os
 import sys
 import phylopandas as ph
 
-file1 = sys.argv[1]
+file1 = sys.argv[1] # input files Go terms and orho groups
 file2 = sys.argv[2]
 
 df = pd.read_csv(file1, sep='\t')
@@ -12,12 +12,10 @@ df.columns = ['id' , 'GOid' , 'GOterm']
 
 dd = ph.read_fasta(file2)
 
-result = pd.merge(dd, df, how='left', on=['id'])
+result = pd.merge(dd, df, how='left', on=['id']) # compare two files
 result = result[['id' , 'GOid', 'GOterm']]
 
 f = len(result.index)
 
 pd.options.display.max_rows = f
 print (result)
-#result = result.dropna()
-#result.to_csv( file2.csv, sep ='\t')
