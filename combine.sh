6caf64fcd5e215dfd1624703328a932824a70748
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 15:31:05 +0700

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-#cat *.fasta > test.fasta
-awk '/^>/ { if (id[$1]++ == 0) printing = 1; else printing = 0 } { if (printing) print }' test.fasta > new.fasta
+cat *.fasta > test.fasta # combine all data bases in one
+awk '/^>/ { if (id[$1]++ == 0) printing = 1; else printing = 0 } { if (printing) print }' test.fasta > new.fasta #filter duplicatication sequenses
 
