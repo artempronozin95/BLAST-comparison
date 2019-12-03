8e5b29ba00afa881884e045787bf35b801e1eb51
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 12:44:23 +0700

    Update run_orto.sh

--- run_orto.sh
+++ run_orto.sh
@@ -3,7 +3,7 @@
 ORT_GROUPS='ortho_group.csv' # file with id gene and id ortho group of this gene
 THR=1 # number of streams  
 RUN='./pro.sh' # script that run automatically
-OUT_DIR='./protein' # savig folder 
+OUT_DIR='./protein' # saving folder 
 
 cat $ORT_GROUPS | awk -F ',' '{print $1}' | tail -n +2 | parallel --verbose --colsep ' ' --jobs $THR "$RUN {1}"
 
