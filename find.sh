6cc11e043eef92bb72192d9bf1c36735572d4fba
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 15:41:59 +0700

    Update find.sh

--- find.sh
+++ find.sh
@@ -3,17 +3,17 @@
 THR=25
 RUN='./test_cfg.pl'
 RUN2='./id_database.pl'
-ALL='/home/pronozinau/OrthoDB/mmseq/*'
-#a=$(find /home/pronozinau/OrthoDB/Viridiplantaelevel -name "$1_*")
-#a=$(find /home/pronozinau/OrthoDB/Eukaryotalevel -name "$1_*")
-b=$(find /home/pronozinau/OrthoDB/protein/ -name "$1.fasta")
-#$RUN2 $ALL $b
-$RUN $b
+ALL='/home/pronozinau/OrthoDB/mmseq/All_euk.fasta'
 
-#wc $1.fasta.Blast.outfmt6 >> $1.fasta.param.csv
-#wc $1.fasta.Dia.outfmt6 >> $1.fasta.param.csv
-#wc $1.fasta.use.outfmt6 >> $1.fasta.param.csv
+b=$(find /home/pronozinau/OrthoDB/protein/ -name "$1.fasta") #searching protein
+$RUN2 $ALL $b #run id_database.pl
+$RUN $b #run test_cfg.pl
+
+wc $1.fasta.Blast.outfmt6 >> $1.fasta.param.csv #form file with input parameters
+wc $1.fasta.Dia.outfmt6 >> $1.fasta.param.csv
+wc $1.fasta.use.outfmt6 >> $1.fasta.param.csv
 wc $1.fasta.mmseqBla.tab >> $1.fasta.param.csv
+wc $1.fasta.uselocal.outfmt6 >> $1.fasta.param.csv
 
-for file in `ls *.fasta.timeB.csv *.fasta.timeD.csv *.fasta.timeU.csv *.fasta.timeM.csv`; do echo $file; cat $file; done > time_all.csv;
+for file in `ls *.fasta.timeB.csv *.fasta.timeD.csv *.fasta.timeU.csv *.fasta.timeM.csv *.fasta.timeUL.csv`; do echo $file; cat $file; done > time_all.csv;
 for file in `ls *.fasta.param.csv`; do echo $file; cat $file; done > param_all.csv;
