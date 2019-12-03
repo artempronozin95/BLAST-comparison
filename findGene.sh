f705685fc8b83dd7ad956b0a600a2dba6a766731
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 16:53:58 +0700

    Update findGene.sh

--- findGene.sh
+++ findGene.sh
@@ -3,9 +3,7 @@
 THR=25
 RUN='python3 metric.py'
 FOLD='/home/pronozinau/OrthoDB/old_gene'
-RUN2='python3 aver.py'
-RUN3='python3 mean.py'
-RUN4='python3 TP_FP.py'
+RUN2='python3 mapk.py'
 RUN5='python3 recall.py'
 
 a=$(find $FOLD -name "$1.fasta.Blast.outfmt6")
@@ -14,6 +12,5 @@ c=$(find $FOLD -name "$1_*.fasta.csv")
 d=$(find $FOLD -name "$1.fasta.use.outfmt6")
 e=$(find $FOLD -name "$1.fasta.BlastFast.outfmt6")
 r=$(find $FOLD -name "$1.csv")
-#$RUN5 "$a" "$b" "$c" "$d" "$e" > $1_Pre.BlaFa.csv
+$RUN5 "$a" "$b" "$c" "$d" "$e" > $1_Pre.BlaFa.csv
 $RUN2 $1_Pre.Bla.csv $1_Pre.Dia.csv $1_Pre.Use.csv $1_Pre.BlaFa.csv
-#$RUN3 All_mean.csv
