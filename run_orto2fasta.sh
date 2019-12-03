5e4e8362b343e8c82b48cd068dbed1f10191af7b
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2019-12-03 13:02:47 +0700

    Add files via upload

---
+++ run_orto2fasta.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+ORT_GROUPS='ortho_group.csv'
+THR=13
+RUN='./orto2fasta.sh'
+OUT_DIR='./tmp'
+
+cat $ORT_GROUPS | awk -F ',' '{print $1" "$2}' | tail -n +2 |  parallel --verbose --colsep ' ' --jobs $THR "$RUN {2} > $OUT_DIR/{1}_{2}_Viridiplantae.fasta"
+cat $ORT_GROUPS | awk -F ',' '{print $1" "$3}' | tail -n +2 | parallel --verbose --colsep ' ' --jobs $THR "$RUN {2} > $OUT_DIR/{1}_{2}_Eukaryotal.fasta"
+
+
