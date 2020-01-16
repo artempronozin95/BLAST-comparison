fb2b369a64052219e9c6d7b6306813251a350609
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-01-16 13:43:05 +0700

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 cat *.fasta > test.fasta # combine all data bases in one
+grep '^>' AllAge.fasta | sort | uniq -c > test.csv # check on duplication of headers in fasta file
 awk '/^>/ { if (id[$1]++ == 0) printing = 1; else printing = 0 } { if (printing) print }' test.fasta > new.fasta #filter duplicatication sequenses
 
