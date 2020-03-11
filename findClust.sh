8adcfd4851bdd9df715993feb04397167ed9f5fb
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:17:32 +0700

    Delete findClust.sh

--- findClust.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-THR=25
-RUN='./clustalw.pl'
-a=$(find /home/pronozinau/OrthoDB/Eukaryotalevel -name "$1_*")
-b=$(find /home/pronozinau/OrthoDB/protein/ -name "$1*")
-"$RUN" $b $a | sort -nrk 3 > $a.csv
-rm *.dnd
-
