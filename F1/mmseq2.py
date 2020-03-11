94accb62ec1cc089436eb326ca37adbcac8b7e43
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:37:49 +0700

    Add files via upload

---
+++ F1/mmseq2.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import ml_metrics as metrics
+import sys
+import os
+
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+n=1
+
+#base = os.path.basename(file3)
+#base = os.path.splitext(base)[0]
+#prot, no = base.split("_", 1)
+
+clustal = pd.read_csv(file2, sep='\t', header=None)
+mmseq = pd.read_csv(file1,sep='\t', header=None)
+
+clustal.columns = ['id_orth', 'id_prot', 'persent']
+mmseq.columns = ['id_prot', 'id_orth', 'persent', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+
+id_orth_clust = clustal['id_orth']
+id_orth_mmseq = mmseq['id_orth']
+
+print ("MMSEQ2")
+def Precision(x,y):
+    if x+y == 0:
+       return float(0)
+    else:
+       return x/(x+y)
+def Sensitivity(x,y):
+    if y == 0:
+       return float(0)
+    else:
+       return x/y
+def F1(x,y):
+    if x+y == 0:
+       return float(0)
+    else:
+       return 2*(x*y)/(x+y)
+def TP_FP(x,y):
+    TP = 0
+    FP = 0
+    FN = 0
+    TN = 0
+    for w in y:
+        if w in x:
+           TP = TP+1
+        else:
+           FP = FP+1
+    Pre = Precision(TP,FP)
+    Sen = Sensitivity(TP,len(x))
+    F = F1(Pre,Sen)
+#    return (Pre , Sen , F1)
+    return (Pre)
+#    return (Sen)
+#    return (F)
+def stop (x,y,z):
+    if z > len(y):
+       sys.exit()
+    else:
+       return TP_FP(x,y)
+
+
+while n <= 100:
+    actual = id_orth_clust.to_list()[:n]
+    mmsqq = id_orth_mmseq.to_list()[:n]
+    mmsq = stop(actual , mmsqq, n)
+    n = n + 1
+    print (mmsq)
+
---
+++ F1/uselocal.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import ml_metrics as metrics
+import sys
+import os
+
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+n=1
+
+#base = os.path.basename(file3)
+#base = os.path.splitext(base)[0]
+#prot, no = base.split("_", 1)
+
+clustal = pd.read_csv(file2, sep='\t', header=None)
+uselocal = pd.read_csv(file1, sep='\t', header=None)
+
+clustal.columns = ['id_orth', 'id_prot', 'persent']
+uselocal.columns = ['id_prot', 'id_orth', 'persent', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+
+id_orth_clust = clustal['id_orth']
+id_orth_useloc = uselocal['id_orth']
+
+print ("USE_LOCAL")
+def Precision(x,y):
+    if x+y == 0:
+       return float(0)
+    else:
+       return x/(x+y)
+def Sensitivity(x,y):
+    if y == 0:
+       return float(0)
+    else:
+       return x/y
+def F1(x,y):
+    if x+y == 0:
+       return float(0)
+    else:
+       return 2*(x*y)/(x+y)
+def TP_FP(x,y):
+    TP = 0
+    FP = 0
+    FN = 0
+    TN = 0
+    for w in y:
+        if w in x:
+           TP = TP+1
+        else:
+           FP = FP+1
+    Pre = Precision(TP,FP)
+    Sen = Sensitivity(TP,len(x))
+    F = F1(Pre,Sen)
+#    return (Pre , Sen , F1)
+    return (Pre)
+#    return (Sen)
+#    return (F)
+def stop (x,y,z):
+    if z > len(y):
+       sys.exit()
+    else:
+       return TP_FP(x,y)
+
+
+while n <= 100:
+    actual = id_orth_clust.to_list()[:n]
+    usealoc = id_orth_useloc.to_list()[:n]
+    uselo = stop(actual, usealoc, n)
+    n = n + 1
+    print (uselo)
+
