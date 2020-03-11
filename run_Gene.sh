5648fcaff2292a7f9792b5479283761551a12ec7
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:18:26 +0700

    Delete run_Gene.sh

--- run_Gene.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-ORT_GROUPS='/home/pronozinau/OrthoDB/old_gene/old.csv'
-THR=25
-RUN='./findGene.sh'
-cat $ORT_GROUPS | awk -F ',' '{print $1}' |parallel --verbose --colsep ' ' --jobs $THR $RUN
-
