39d2433a62edd9ee2e24faed1753a423cd12d658
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:18:40 +0700

    Delete run_clustalw.sh

--- run_clustalw.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ORT_GROUPS='/home/pronozinau/OrthoDB/ortho_group.csv'
-THR=25
-RUN='./findGO.sh'
-OUT_DIR='/home/pronozinau/OrthoDB/Eukaryotalevel'
-OUT_PROT='/home/pronozinau/OrthoDB/protein'
-cat $ORT_GROUPS | awk -F ',' '{print $1}' |parallel --verbose --colsep ' ' --jobs $THR $RUN
-
