b0fae7420393c197a8e96552d1422711b9bc1b90
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:16:14 +0700

    Delete run_orto.sh

--- run_orto.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ORT_GROUPS='ortho_group.csv' # file with id gene and id ortho group of this gene
-THR=1 # number of streams  
-RUN='./pro.sh' # script that run automatically
-OUT_DIR='./protein' # saving folder 
-
-cat $ORT_GROUPS | awk -F ',' '{print $1}' | tail -n +2 | parallel --verbose --colsep ' ' --jobs $THR "$RUN {1}"
-
