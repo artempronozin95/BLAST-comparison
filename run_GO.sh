3e6881cc0a8c10b7bc742bf9966bd269a544d9b5
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:16:05 +0700

    Delete run_GO.sh

--- run_GO.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-ORT_GROUPS='/home/pronozinau/OrthoDB/ortho_group.csv'
-THR=25
-RUN='./findGO.sh'
-cat $ORT_GROUPS | awk -F ',' '{print $1}' |parallel --verbose --colsep ' ' --jobs $THR $RUN
-
