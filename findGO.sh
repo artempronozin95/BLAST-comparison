40f67434c455a3992ee6fa0212835c5712655d5f
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-03-11 13:17:38 +0700

    Delete findGO.sh

--- findGO.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-ORT='/home/pronozinau/OrthoDB/odb10v0_gene_xrefs_onlyGO.tab'
-THR=25
-RUN='python3 column.py'
-FOLD='/home/pronozinau/OrthoDB/Eukaryotalevel'
-a=$(find $FOLD -name "$1_*")
-$RUN $ORT "$a" > "$a"_GO.csv
-
