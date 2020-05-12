967f5826697a88208bd6fbe6fe8ec7611fb85a68
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-05-12 15:53:19 +0700

    Delete rm.sh

--- rm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-rm *.fasta.timeB.csv *.fasta.timeD.csv *.fasta.timeU.csv *.fasta.timeBF.csv *.fasta.timeM.csv *.fasta.timeUL.csv
-rm *.fasta.param.csv
-rm ./metout/*.fasta.Dia.daa
-rm ./metout/*.fasta.mmseq.outfmt6
