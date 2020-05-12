af438d3754e69669b96af075b5a970f865ecf093
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2020-05-12 15:44:40 +0700

    Update Methods.sh

--- Methods.sh
+++ Methods.sh
@@ -3,17 +3,16 @@
 THR=5
 RUN1='./methods_run.pl'
 metout='./metout'
-LIB='./library/All_libC.fasta'
-ublast='./library/All_libC.fasta.udb'
-local='./library/All_libC.fasta.use.udb'
+LIB='./library/odb10_all_fasta.tab'
+ublast='./library/odb10_all_fasta.tab.udb'
+local='./library/odb10_all_fasta.tab.use.udb'
 RUN2='./database_build.pl'
 lib='./library'
-ALL='./All_libC.fasta'
+ALL='./odb10_all_fasta.tab'
+b='./Protein.fasta'
 
 mkdir $lib
 mkdir $metout
-
-b=$(find ./protein -name "$1.fasta")
 
 $RUN2 $ALL $b
 $RUN1 $b $LIB $ublast $local
